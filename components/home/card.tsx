3652e13fc17d4b51f29d37b9335d1c3bc4adcc69
GustavoEda <edapd12@gmail.com>
2024-09-10 19:38:35 -0300

    Update card.tsx

--- components/home/card.tsx
+++ components/home/card.tsx
@@ -68,6 +68,23 @@ export default function Card({
             </ReactMarkdown>
           </Balancer>
         </div>
+         <div className="leading-normal text-gray-500" style={{marginBottom: "20%"}}>
+          <Balancer>
+            <ReactMarkdown
+              components={{
+                a: ({ node, ...props }) => (
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    {...props}
+                    className="font-medium text-gray-800 underline transition-colors"
+                  />
+                ),
+              }}
+            >
+            </ReactMarkdown>
+          </Balancer>
+        </div>
         {!isSelected ? (
           <div
             style={{
