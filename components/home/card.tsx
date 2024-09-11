bf979ce91d2c4eb50a5c75d044f94786c232ebee
GustavoEda <edapd12@gmail.com>
2024-09-10 21:26:10 -0300

    Update card.tsx

--- components/home/card.tsx
+++ components/home/card.tsx
@@ -23,93 +23,100 @@ export default function Card({
 }) {
   const [isSelected, setIsSelected] = useState(Boolean(selected));
   const { DemoModal, setShowDemoModal } = useDemoModal({ title, id, setIsSelected });
-  return (
-    <div
-      className={`relative col-span-1 overflow-hidden rounded-xl bg-white shadow-md`}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-        width: "100%",
-        margin: 0,
-      }}
-    >
-      <DemoModal />
-      <div className="flex h-60 items-center justify-center">{demo}</div>
-      <div className="mx-auto max-w-md text-center" style={{ flexGrow: 1, margin: 0 }}>
-        <h2
-          style={{
-            animationDelay: "0.15s",
-            animationFillMode: "forwards",
-            color: "#531D13",
-          }}
-          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text px-1 font-display text-xl font-bold text-transparent md:text-3xl md:font-bold"
-        >
-          <Balancer>{title}</Balancer>
-        </h2>
-        <h2 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text px-1 pt-3 font-display text-xl font-bold text-transparent md:text-2xl md:font-normal">
-          <Balancer>R${parseFloat(price).toFixed(2).replace('.', ',')}</Balancer>
-        </h2>
-        <div className="prose-sm leading-normal text-gray-500 md:prose">
-          <Balancer>
-            <ReactMarkdown
-              components={{
-                a: ({ node, ...props }) => (
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    {...props}
-                    className="font-medium text-gray-800 underline transition-colors"
-                  />
-                ),
-              }}
-            >
-              {description}
-            </ReactMarkdown>
-          </Balancer>
-        </div>
-         <div className="leading-normal text-gray-500" style={{marginBottom: "20%"}}>
-          <Balancer>
-          </Balancer>
-        </div>
-        {!isSelected ? (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              position: "absolute",
-              width: "100%",
-              maxWidth: "none",
-              bottom: 0,
-            }}>
-            <button
-              type="button"
-              className="mb-3 rounded-lg bg-olive px-5 py-2.5 text-center text-sm font-medium text-cream shadow-lg"
-              onClick={() => setShowDemoModal(true)}
-            >
-              Vou dar esse!
-            </button>
-          </div>
-        ) : (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              position: "absolute",
-              width: "100%",
-              maxWidth: "none",
-              bottom: 0,
-            }}>
-            <button
-              disabled
-              type="button"
-              className="text-dark-brown mb-3 cursor-not-allowed rounded-lg bg-orange px-5 py-2.5 text-center text-sm font-medium shadow-lg"
-            >
-              Já foi escolhido
-            </button>
-          </div>
-        )}
+return (
+  <div
+    className="relative col-span-1 overflow-hidden rounded-xl bg-white shadow-md"
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
+      width: "100%",
+      margin: 0,
+    }}
+  >
+    <DemoModal />
+
+    <div className="flex h-60 items-center justify-center">{demo}</div>
+    
+    <div className="mx-auto max-w-md text-center" style={{ flexGrow: 1, margin: 0 }}>
+      <h2
+        style={{
+          animationDelay: "0.15s",
+          animationFillMode: "forwards",
+          color: "#531D13",
+        }}
+        className="bg-gradient-to-br from-black to-stone-500 bg-clip-text px-1 font-display text-xl font-bold text-transparent md:text-3xl md:font-bold"
+      >
+        <Balancer>{title}</Balancer>
+      </h2>
+      
+      <h2 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text px-1 pt-3 font-display text-xl font-bold text-transparent md:text-2xl md:font-normal">
+        <Balancer>R${parseFloat(price).toFixed(2).replace('.', ',')}</Balancer>
+      </h2>
+
+      <div className="prose-sm leading-normal text-gray-500 md:prose">
+        <Balancer>
+          <ReactMarkdown
+            components={{
+              a: ({ node, ...props }) => (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  {...props}
+                  className="font-medium text-gray-800 underline transition-colors"
+                />
+              ),
+            }}
+          >
+            {description}
+          </ReactMarkdown>
+        </Balancer>
       </div>
+      
+      <div className="leading-normal text-gray-500" style={{marginBottom: "20%"}}>
+        <Balancer></Balancer>
+      </div>
+
+      {!isSelected ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            position: "absolute",
+            width: "100%",
+            maxWidth: "none",
+            bottom: 0,
+          }}
+        >
+          <button
+            type="button"
+            className="mb-3 rounded-lg bg-olive px-5 py-2.5 text-center text-sm font-medium text-cream shadow-lg"
+            onClick={() => setShowDemoModal(true)}
+          >
+            Vou dar esse!
+          </button>
+        </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            position: "absolute",
+            width: "100%",
+            maxWidth: "none",
+            bottom: 0,
+          }}
+        >
+          <button
+            disabled
+            type="button"
+            className="text-dark-brown mb-3 cursor-not-allowed rounded-lg bg-orange px-5 py-2.5 text-center text-sm font-medium shadow-lg"
+          >
+            Já foi escolhido
+          </button>
+        </div>
+      )}
     </div>
-  );
+  </div>
+);
 }
