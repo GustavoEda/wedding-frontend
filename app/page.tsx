4e15395da06290cfbfe7d43196e37b4446eda549
GustavoEda <edapd12@gmail.com>
2024-09-12 20:39:01 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ data.map((gift: any) => {
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
     <h1
-      className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text pb-3 text-center font-display text-3xl sm:text-4xl md:text-7xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:leading-[5rem]"
+      className="animate-fade-up bg-gradient-to-br bg-clip-text pb-3 text-center font-display text-3xl sm:text-4xl md:text-7xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:leading-[5rem]"
       style={{
         animationDelay: "0.15s",
         animationFillMode: "forwards",
@@ -52,7 +52,7 @@ data.map((gift: any) => {
       <Balancer style={{ fontFamily: 'cursive', fontSize: '24px' }}>Gustavo & Renata</Balancer>
     </h1>
         <p
-            className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
+            className="mt-6 animate-fade-up text-center opacity-0 md:text-xl"
             style={{ 
               animationDelay: "0.25s", 
               animationFillMode: "forwards",
